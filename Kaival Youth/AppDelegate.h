b5055137643f2c3f1a9620ab6d40f51fd42cb3b5
Janki Patel <janipatel07@gmail.com>
2015-08-07 20:50:20 -0500

    Initial Commit

---
+++ Kaival Youth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kaival Youth
+//
+//  Created by Janki Patel on 8/7/15.
+//  Copyright (c) 2015 Janki Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kaival Youth/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Kaival Youth
+//
+//  Created by Janki Patel on 8/7/15.
+//  Copyright (c) 2015 Janki Patel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
